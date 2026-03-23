4e60cf4b706fea7cc384da42a175c1623a212ef4
Neevaik <kevin.mavier@hotmail.com>
2026-03-23 17:28:14 +0100

    change model cart

--- models/Cart.js
+++ models/Cart.js
@@ -1,19 +1,37 @@
 const mongoose = require("mongoose");
 
-const cartSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User", required: true
+const cartItemSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true
     },
-    items: [
-        {
-            product: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Product"
-            },
-            quantity: { type: Number, default: 1 }
-        }
-    ]
-}, { timestamps: true });
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+      min: 1
+    }
+  },
+  { _id: false }
+);
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+      index: true
+    },
+    items: {
+      type: [cartItemSchema],
+      default: []
+    }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Cart", cartSchema);
