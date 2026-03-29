d35550557dd47a3ccbe1eeb4ac8633a0cede85fb
Neevaik <kevin.mavier@hotmail.com>
2026-03-29 10:11:48 +0200

    checkUser when signup

--- middlewares/verifications.js
+++ middlewares/verifications.js
@@ -87,4 +87,30 @@ function requireRole(...roles) {
   };
 }
 
-module.exports = { verifyBody, verifyToken, requireRole };
+async function verifyExistingUser(req, res, next) {
+  try {
+    let { username } = req.body;
+
+    username = username.trim();
+    if (!username) {
+      return res.status(400).json({
+        ok: false,
+        message: "❌ Username is required"
+      });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).json({
+        ok: false,
+        message: "❌ Username already exists"
+      });
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { verifyBody, verifyToken, requireRole, verifyExistingUser };
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { verifyBody, verifyToken, requireRole } = require("../middlewares/verifications");
+const { verifyBody, verifyToken, requireRole, verifyExistingUser } = require("../middlewares/verifications");
 const { signup, login, logout, updateUser, deleteUser } = require("../controllers/authController");
 
 router.post("/login", verifyBody(["username", "password"]), login)
-router.post("/signup", verifyBody(["username", "password", "email"]), signup)
+router.post("/signup", verifyBody(["username", "password", "email"]), verifyExistingUser, signup)
 router.post("/logout", logout)
 router.put("/:id", verifyToken, requireRole("admin"), updateUser);
 router.delete("/:id", verifyToken, requireRole("admin"), deleteUser);
