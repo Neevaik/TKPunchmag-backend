e7300f0d8a3681406dbefab216740a636f4d38fd
Neevaik <kevin.mavier@hotmail.com>
2026-05-07 16:29:36 +0200

    add credential cors

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ app.use("/payment", paymentRoutes);
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
 
 app.use("/user", usersRoutes);
 app.use("/product", productRoutes);
